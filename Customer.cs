9f76728e0066239bb883f34c342d99bd2450b1a0
adamritadas <amrita.das@student.fh-kiel.de>
2018-06-26 16:54:24 -0700

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProductMangement
+{
+
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        
+        public string CustometName { get; set; }
+
+    }
+
+}
